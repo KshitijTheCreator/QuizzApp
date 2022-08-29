a6b38561cb19bcfc332e2a26e966862e8c2503fe
kshitij <kshitij.june25@gmail.com>
2022-08-30 01:38:38 +0530

    Questions and options structure created

--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.quizzapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val etName: EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+            if (etName.text.isEmpty()) {
+                Toast.makeText(this, "Enter Your Name Before Proceeding..", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
